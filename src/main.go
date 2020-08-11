f9601d4580aa9ba7f06e96c3ee7e82f1d1742878
Setsuna F. Seiei <takraw_s@hotmail.com>
2020-08-12 00:47:15 +0700

    update

--- src/main.go
+++ src/main.go
@@ -15,15 +15,15 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"_info":          "go-jwt-token",
-			"authentication": "payload: username, password",
-			"refresh":        "payload: refresh_token",
-			"getdata":        "payload: access_token, refresh_token",
+			"_info":                "go-jwt-token",
+			"post /authentication": "payload: username, password",
+			"post /token/refresh":  "payload: token: (refresh_token)",
+			"post /getdata":        "payload: token: (access_token), (refresh_token)",
 		})
 	})
 
 	r.POST("/authentication", handlers.PostAuthentication())
-	r.POST("/refresh", handlers.PostValidateToken())
+	r.POST("/token/refresh", handlers.PostValidateToken())
 	r.POST("/getdata", handlers.PostCheckClaims())
 
 	r.Run()
